34f6c7514518cb128c01e3869f93cd703c80bc34
Đỗ Văn Bảo <19521238@gm.uit.edu.vn>
2021-12-29 11:52:07 +0700

    Structure base

---
+++ EmployeeService/tests/EmployeeService.Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace EmployeeService.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ EmployeeService/tests/EmployeeService.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace EmployeeService.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
